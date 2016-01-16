95c205150ac3369a88482a5f749ece0b82f40e65
guantao.hu@jimubox.com <18146525429@163.com>
2016-01-16 16:34:05 +0800

    modify views

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var db = require('mysql');
 var con = db.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'helloworld'
+  password: ''
 });
 con.connect();
 var DBName = 'WebDevelopment';
